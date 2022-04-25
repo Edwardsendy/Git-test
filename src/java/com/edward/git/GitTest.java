eb6efbdf9009785b5ece069a39d4c57cff7fc660
edward <xsd1115637240@gmail.com>
2022-04-25 20:48:02 +0800

    Second commit!

--- src/java/com/edward/git/GitTest.java
+++ src/java/com/edward/git/GitTest.java
@@ -3,5 +3,13 @@ package com.edward.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello java!!!");
+        System.out.println("hello aaa!!!");
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
+
     }
 }
